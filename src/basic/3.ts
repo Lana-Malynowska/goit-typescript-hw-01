64b46ab7b05c4b797d704089ae59e27cccf0ac12
Svitlana Malynowska <lana.malynowska@gmail.com>
2025-05-23 16:38:09 +0300

    solve Basic tasks

--- src/basic/3.ts
+++ src/basic/3.ts
@@ -1,1 +1,2 @@
-// Створіть змінну, яка може містити або рядок, або число (union type)? Також, оголосіть змінну, яка може містити лише одне з двох можливих рядкових значень: 'enable' або 'disable' (literal type).
+let mixedType: string | number;
+let status: "enable" | "disable";
